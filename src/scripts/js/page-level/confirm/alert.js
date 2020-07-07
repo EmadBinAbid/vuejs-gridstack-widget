f5e7c547e5b7d11db93cce5e66a447b74c071f0a
Emad Bin Abid <emad.bin.abid@gmail.com>
2020-07-07 19:00:14 +0500

    Added custom js and css.

---
+++ src/scripts/js/page-level/confirm/alert.js
@@ -0,0 +1,13 @@
+import $ from 'jquery'
+
+$(document).on("click",".alert",function(e){
+      $.alert({
+          title: 'Alert!',
+          content: 'Simple alert!',
+          confirm: function(){
+              alert('Confirmed!');
+          }
+      });
+
+      e.preventDefault();
+});
